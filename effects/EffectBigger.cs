fc2bebef0b90efdbbb1c07840a7ee834b6b4490f
rogeraabbccdd <rogeraabbccdd@gmail.com>
2024-07-10 12:46:43 +0800

    Add 2 scale effect
    More scale and less scale.

---
+++ effects/EffectBigger.cs
@@ -0,0 +1,46 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using Preach.CS2.Plugins.RollTheDiceV2.Core.BaseEffect;
+using Preach.CS2.Plugins.RollTheDiceV2.Utilities;
+
+namespace Preach.CS2.Plugins.RollTheDiceV2.Effects;
+
+public class EffectMoreScale : EffectBaseRegular, IEffectParameter
+{
+    public override bool Enabled { get; set; } = true;
+    public override string PrettyName { get; set; } = "Bigger";
+    public override string TranslationName { get; set; } = "bigger";
+    public override double Probability { get; set; } = 3;
+    public Dictionary<string, string> RawParameters {get; set; } = new();
+    public override bool ShowDescriptionOnRoll { get; set; } = false;
+
+    public override void Initialize()
+    {
+        RawParameters.Add("scale", "1.2");
+    }
+
+    public override void OnApply(CCSPlayerController? playerController)
+    {
+        if (playerController == null || 
+            playerController.PlayerPawn.Value == null ||
+            playerController.PlayerPawn.Value.Entity == null
+        )  return;
+
+        if(!RawParameters.TryGetValue("scale", out var scale))
+            return;
+
+        playerController.PlayerPawn.Value.Entity.EntityInstance.AddEntityIOEvent("SetScale", null, null, scale);
+
+        PrintDescription(playerController, TranslationName, scale);
+    }
+
+    public override void OnRemove(CCSPlayerController? playerController)
+    {
+        if (playerController == null || 
+            playerController.PlayerPawn.Value == null ||
+            playerController.PlayerPawn.Value.Entity == null
+        )  return;
+
+        playerController.PlayerPawn.Value.Entity.EntityInstance.AddEntityIOEvent("SetScale", null, null, "1");
+    }
+}
